b2f1b073fccad430efde9973fe90866437a1a034
NTTDATA <touyoudai145@yahoo.co.jp>
2013-07-24 11:08:17 +0900

    init

--- ApnsPHP/sample_push.php
+++ ApnsPHP/sample_push.php
@@ -28,53 +28,40 @@ error_reporting(-1);
 // Using Autoload all classes are loaded on-demand
 require_once 'ApnsPHP/Autoload.php';
 
-// Instanciate a new ApnsPHP_Push object
+// 開発用かリリース用かの設定とpemファイルの指定
 $push = new ApnsPHP_Push(
 	ApnsPHP_Abstract::ENVIRONMENT_PRODUCTION,
 	'server_certificates_bundle_sandbox_production.pem'
 );
 
-// Set the Root Certificate Autority to verify the Apple remote peer
+//アップルリモートピアを検証するために、pemを指定する
 $push->setRootCertificationAuthority('entrust_root_certification_authority.pem');
 
-// Connect to the Apple Push Notification Service
 $push->connect();
 
-// Instantiate a new Message with a single recipient
+// デバイストークンの設定
 $message = new ApnsPHP_Message('fe85afc916011120e607e0f881723ad8c7fc6a396be1ccfadc2f46282468e7d1');
 
-// Set a custom identifier. To get back this identifier use the getCustomIdentifier() method
-// over a ApnsPHP_Message object retrieved with the getErrors() message.
 $message->setCustomIdentifier("Message-Badge-3");
 
-// Set badge icon to "3"
 $message->setBadge(3);
 
-// Set a simple welcome text
 $message->setText('Hello APNs-enabled device!');
 
-// Play the default sound
 $message->setSound();
 
-// Set a custom property
 $message->setCustomProperty('acme2', array('bang', 'whiz'));
 
-// Set another custom property
 $message->setCustomProperty('acme3', array('bing', 'bong'));
 
-// Set the expiry value to 30 seconds
 $message->setExpiry(30);
 
-// Add the message to the message queue
 $push->add($message);
 
-// Send all messages in the message queue
 $push->send();
 
-// Disconnect from the Apple Push Notification Service
 $push->disconnect();
 
-// Examine the error message container
 $aErrorQueue = $push->getErrors();
 if (!empty($aErrorQueue)) {
 	var_dump($aErrorQueue);
